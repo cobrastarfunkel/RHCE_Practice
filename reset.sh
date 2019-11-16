c63b8d24f0e071dd899762e71e8e7cd8db68d3cd
cobrastarfunkel <iancobia@gmail.com>
2019-11-16 10:45:03 -0600

    Cleaned up comments

--- reset.sh
+++ reset.sh
@@ -4,9 +4,9 @@
 
 help_text="$(basename "$0") Usage: [-h] [-n] [-f] -- Resets various settings used for RHCE prep to make practice more efficient
 
-	-h print this text
-	-n reset network interface scripts (you will have no connections or routes)
-	-f remove firewall rules that aren't part of the default (ssh, dhcpv6-client)"
+    -h print this text
+    -n reset network interface scripts (you will have no connections or routes)
+    -f remove firewall rules that aren't part of the default (ssh, dhcpv6-client)"
 
 [ $# -eq 0 ] && { echo "${help_text}"; exit 1; }
 
@@ -16,16 +16,16 @@ help_text="$(basename "$0") Usage: [-h] [-n] [-f] -- Resets various settings use
 # Reset Newtork configs by removing ifcfg files
 ################################################
 reset_network() {
-	# Clear out Network Configs
-	find /etc/sysconfig/network-scripts/ -not -regex '.*ifcfg-[lo].*' -regex '.*ifcfg-.*' -exec rm {} +
+    # Clear out Network Configs
+    find /etc/sysconfig/network-scripts/ -not -regex '.*ifcfg-[lo].*' -regex '.*ifcfg-.*' -exec rm {} +
 
     # Clear Routes
     ip route flush all
 
-	# restart network and reload network manager
-	systemctl restart network;
-	nmcli con reload
-	printf "Network configs reset!\n"
+    # restart network and reload network manager
+    systemctl restart network;
+    nmcli con reload
+    printf "Network configs reset!\n"
 }
 
 
@@ -49,22 +49,22 @@ reset_firewalld() {
 
 
 while getopts :hnf opt; do
-	case $opt in
-		h)
-			echo "{$help_text}"
-			exit
-			;;
-		n)
-			echo "Resetting Network Configs"
-			reset_network
-			;;
-		f)
-			echo "Resetting Firewall"
-			reset_firewalld
-			;;
-		\?)	echo "{$help_text}"
-			;;
-	esac
+    case $opt in
+        h)
+            echo "{$help_text}"
+            exit
+            ;;
+        n)
+            echo "Resetting Network Configs"
+            reset_network
+            ;;
+        f)
+            echo "Resetting Firewall"
+            reset_firewalld
+            ;;
+        \?)	echo "{$help_text}"
+            ;;
+    esac
 done
 
 
