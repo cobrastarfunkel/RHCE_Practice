b2ae4c0dbfccd4549c37f5256421a85eb691a624
cobrastarfunkel <iancobia@gmail.com>
2019-11-16 10:42:04 -0600

    Cleaned up comments

--- reset.sh
+++ reset.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# Reset things done to practice for RHCE
 
 help_text="$(basename "$0") Usage: [-h] [-n] [-f] -- Resets various settings used for RHCE prep to make practice more efficient
 
@@ -6,9 +8,13 @@ help_text="$(basename "$0") Usage: [-h] [-n] [-f] -- Resets various settings use
 	-n reset network interface scripts (you will have no connections or routes)
 	-f remove firewall rules that aren't part of the default (ssh, dhcpv6-client)"
 
-[ $# -eq 0 ] && { echo "$help_text"; exit 1; }
+[ $# -eq 0 ] && { echo "${help_text}"; exit 1; }
 
 
+
+################################################
+# Reset Newtork configs by removing ifcfg files
+################################################
 reset_network() {
 	# Clear out Network Configs
 	find /etc/sysconfig/network-scripts/ -not -regex '.*ifcfg-[lo].*' -regex '.*ifcfg-.*' -exec rm {} +
@@ -24,11 +30,16 @@ reset_network() {
 
 
 
+################################################
+# Reset firewalld Zones to default settings.
+#
+# Deleteing the zone files will set the zones 
+# back to default values.
+# Defaults are defined in /usr/lib/firewalld.
+# Once you add a rule a new file will be created 
+# in /etc/firewalld/zones
+################################################
 reset_firewalld() {
-	# Reset firewalld Zones to default
-    # Deleteing the zone files will set the zones back to default values
-    # Defaults are defined in /usr/lib/firewalld
-    # Once you add a rule a new file will be created in zones
     yes| rm -I /etc/firewalld/zones/* 2> /dev/null
     firewall-cmd --reload
     printf "Firewall rules reset\n"
@@ -40,7 +51,7 @@ reset_firewalld() {
 while getopts :hnf opt; do
 	case $opt in
 		h)
-			echo "$help_text"
+			echo "{$help_text}"
 			exit
 			;;
 		n)
@@ -51,7 +62,7 @@ while getopts :hnf opt; do
 			echo "Resetting Firewall"
 			reset_firewalld
 			;;
-		\?)	echo "$help_text"
+		\?)	echo "{$help_text}"
 			;;
 	esac
 done
