d0bf4c7136f404458966a3d684930bc50971e6ac
Ian <iancobia@gmail.com>
2019-12-13 06:06:34 -0500

    Soe vimrc changes and PS1, reset removes partitions that are defined

--- ansible/roles/common/files/99_prompt.sh
+++ ansible/roles/common/files/99_prompt.sh
@@ -1,1 +1,6 @@
-export PS1="\[\033[0;35m\]\u@\h \w> \[\033[0;34m\]"
+if [ $TERM = "xterm" ]; then
+  export PS1="\[\033[0;35m\]\u@\h \[\033[0;34m\]\w \[\033[00m\]> "
+else
+  # Above makes it hard to read on the proxmox terminal
+  export PS1="\[\033[0;35m\]\u@\h \[\033[0;32m\]\w \[\033[00m\]> "
+fi
