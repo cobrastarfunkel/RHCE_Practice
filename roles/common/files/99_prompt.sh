29564446dae2b54e9d4738ae34c99156c83d944a
Ian <iancobia@gmail.com>
2019-12-24 14:21:14 -0500

    Trying to fix vim colors in screen but not working

--- roles/common/files/99_prompt.sh
+++ roles/common/files/99_prompt.sh
@@ -1,4 +1,6 @@
-if [ $TERM = "xterm" ]; then
+if [[ $TERM =~ xterm* ]] || [ $TERM = "screen" ]; then
+  # Work around for screen changing vim colors
+  alias screen="TERM=xterm-256color screen"
   export PS1="\[\033[0;35m\]\u@\h \[\033[0;34m\]\w \[\033[00m\]> "
 else
   # Above makes it hard to read on the proxmox terminal
